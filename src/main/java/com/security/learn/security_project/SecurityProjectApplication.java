f70bb9ea8479ffa9737a6b3eb4ab95aad2d52481
shoaib <shasanex@gmail.com>
2025-10-17 17:25:54 +0530

    initial commit

---
+++ src/main/java/com/security/learn/security_project/SecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.security.learn.security_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProjectApplication.class, args);
+	}
+
+}
